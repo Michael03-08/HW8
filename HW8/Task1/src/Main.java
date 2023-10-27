96ccef8b653040264b7d2e03bdb0d0deb3614fc0
Flex <cma491628@gmail.com>
2023-10-27 16:11:01 +0300

    Урок восьмой рандомные массивы

--- HW8/Task1/src/Main.java
+++ HW8/Task1/src/Main.java
@@ -2,31 +2,23 @@ public class Main {
     public static void main(String[] args) {
         //#1 Создать массив из 10 целых чисел. Значения могут быть любыми
         //todo
-        int[] array = {5, 6, 7, 12, 15, 23, 14, 7, 9, 52 };
+        int[] array = {1, 2, 3, 4, 5, 6, 7,8, 9, 10 };
 
         //#2 Распечатать все значения массива начиная с 0 до последнего индексов.
-        //todo
+
         for (int i = 0; i < array.length; i++){
             System.out.println(array[i]);
         }
 
         //#3 Распечатать все значения массива с конца (начиная с последнего до 0 индексов).
-        //todo
-        System.out.println("Массив распечатка с конца: ");
-        System.out.println(array[9]);
-        System.out.println(array[8]);
-        System.out.println(array[7]);
-        System.out.println(array[6]);
-        System.out.println(array[5]);
-        System.out.println(array[4]);
-        System.out.println(array[3]);
-        System.out.println(array[2]);
-        System.out.println(array[1]);
-        System.out.println(array[0]);
+
+        System.out.println("Распечатываем массив с конца: ");
+        for (int i = array.length - 1; i >= 0; i--){
+            System.out.println(array[i]);
+        }
 
         //#4 *** Задание особой сложности. Развернуть массив(см конец урока).
-        //todo
-
+        System.out.println("Перевернутый массив: ");
         int left = 0;
         int right = array.length - 1;
 
@@ -38,7 +30,7 @@ public class Main {
             right--;
         }
         for(int i = 0; i < array.length; i++){
-            System.out.println("Перевернутый массив: " + array[i]);
+            System.out.println( array[i]);
         }
     }
 }
