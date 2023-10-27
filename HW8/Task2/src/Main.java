de73ac519265e28bb980e3543ee1a44f86db5d05
Flex <cma491628@gmail.com>
2023-10-27 16:36:03 +0300

    Урок восьмой рандомные массивы

--- HW8/Task2/src/Main.java
+++ HW8/Task2/src/Main.java
@@ -4,35 +4,35 @@ public class Main {
     public static void main(String[] args) {
         //todo код программы писать тут
 
-        int[] weight = new int[10];
+        float[] weight = new float[10];
 
         int min = 40;
         int max = 100;
-        int count = 0;
+        float count = 0;
         int min1 = 60;
         int max1 = 80;
 
         Random random = new Random();
         for (int i = 0; i < weight.length; i++){
-            weight[i] = random.nextInt(max - min + 1) + min;
+            weight[i] = random.nextFloat(max - min + 1) + min;
         }
         for (int i = 0; i < weight.length; i++){
             System.out.println(weight[i]);
         }
 
-        double sum = 0;
+        float sum = 0;
         for (int i = 0; i < weight.length; i++){
             sum += weight[i];
         }
         System.out.println("Средний вес: " + sum / weight.length);
 
 
-        for (int num : weight){
+        for (float num : weight){
             if (num >= min1 && num <= max1){
                 count++;
             }
         }
 
-        System.out.println("Количество людей с ввесом в диапвзоне 0т 60 до 80 кг: " + count);
+        System.out.println("Количество людей с ввесом в диапвзоне от 60 до 80 кг: " + count);
     }
 }
